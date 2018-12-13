605234fcf91864c90d1522ce5a159002cfd80ded
Moltao <kees.vandoorn@gmail.com>
2018-12-13 23:13:00 +0100

    price and name added, also throttling

--- Brouwschraper/brouwen/settings.py
+++ Brouwschraper/brouwen/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 2.5
+# DOWNLOAD_DELAY = 2.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -70,14 +70,14 @@ DOWNLOAD_DELAY = 2.5
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 8
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 30
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
--- Brouwschraper/brouwen/spiders/brouw_spider.py
+++ Brouwschraper/brouwen/spiders/brouw_spider.py
@@ -13,7 +13,18 @@ class brouwspider(Spider):
             yield response.follow(url, callback=self.parse_details)
 
     def parse_details(self, response):
-       prod_details = response.xpath('//div[@class="std"]/ul//li/text()').extract()
-       d1 = {k[0]:k[1].strip() for k in [x.split(":") for x in prod_details]}
-       d1['Name'] = response.xpath('//h1[@itemprop="name"]/text()').extract()[0]
-       yield d1
+        prod_details = response.xpath('//div[@class="std"]/ul//li/text()').extract()
+       
+        d1 = {k[0]:k[1].strip() for k in [x.split(":") for x in prod_details]}
+       
+        d1['Name'] = response.xpath('//h1[@itemprop="name"]/text()').extract()[0]
+       
+        #checken op aanbieding en prijs opslaan
+        if response.xpath('.//p[@class="special-price"]'):
+            prijs = response.xpath('.//p[@class="special-price"]/span/text()').extract()[0].strip()
+        else:
+            prijs = response.xpath('.//span[@class="price"]/text()').extract()[0].strip()
+        
+        d1['Price'] = prijs
+
+        yield d1
